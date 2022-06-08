a6215f8f8c3a8e9c2fd3a06be7016bc09561e140
firefileert <107062774+firefileert@users.noreply.github.com>
2022-06-08 03:21:48 +0000

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'fhufhru45.herokuapp.com'
+      url.hostname = 'dgy5ujujl.herokuapp.com'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
